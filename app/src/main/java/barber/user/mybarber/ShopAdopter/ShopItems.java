875000ef4a786b9c81c0c44b1ee33ac23247f79b
Pramod <pramodchaurasia51@gmail.com>
2021-02-08 00:43:46 +0530

    8Feb21

--- app/src/main/java/barber/user/mybarber/ShopAdopter/ShopItems.java
+++ app/src/main/java/barber/user/mybarber/ShopAdopter/ShopItems.java
@@ -1,7 +1,57 @@
 package barber.user.mybarber.ShopAdopter;
 
 public class ShopItems {
-    String shopName,shopId,;
+    String id, name, owner, phone, address;
 
+    public ShopItems(String id, String name, String owner, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.phone = phone;
+        this.address = address;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> origin/master
